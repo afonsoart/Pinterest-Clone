26f63b5e954a2bfb1e8b80c6bbfe19baeef7d18b
Afonso Ramon de Araujo Teles <afonso1907@hotmail.com>
2022-08-13 10:44:42 -0300

    estrutura base

---
+++ script.js
@@ -0,0 +1,16 @@
+const grids = document.querySelectorAll('.grid')
+const headings = document.querySelectorAll('.heading .wrapper .text')
+
+function enterScreen(index) {
+
+}
+
+function exitScreen(index, ) {
+
+}
+
+function setupAnimationCycle({ initialScreenIndex, timePerScreen, exitDelay })
+
+setupAnimationCycle({
+    initialScreenIndex: 0,
+})
